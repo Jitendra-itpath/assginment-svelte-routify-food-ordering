327281e8865d5686804d610e44e5b1b4857c79d2
Jitendra Chaudhary <jitendrac.itpath@gmail.com>
2023-04-24 18:27:51 +0530

    # - 244 implemented store and add feedback & products with validation

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,1 +1,22 @@
-<h1>Index</h1>
+<script lang="ts">
+     import { productInfo } from '../stores/StoresData';
+     import * as _ from 'lodash'
+     let productData = $productInfo;
+     productData = _.reverse(productData)
+</script>
+
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:mx-5 md:mr-18 mx-3"> 
+     {#each productData as product}
+	     <div class="bg-white rounded-lg overflow-hidden shadow-md">
+		     <img src="{product.productImage}" alt="Product Image" class="w-full h-52 object-cover">
+			<div class="px-6 py-4">
+				<a href="/#">
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{product.productName}</h5>
+				</a>
+				<p class="mb-3 font-bold  text-red-900">{product.productPrice}</p>
+				<p class="mb-3 font-normal text-gray-700">{product.productDescription}</p>
+			</div>
+		</div>
+     {/each}
+</div>
---
+++ src/validation/productSuite.ts
@@ -0,0 +1,38 @@
+import { create, test, enforce, only } from 'vest';
+  
+const productSuite = create((data = {}, currentField) => {
+
+  only(currentField);
+  
+  test('name', 'This field is required.', () => {
+    enforce(data.name).isNotBlank();
+  });
+
+  test('name', 'Dish name must be at least 3 characters.', () => {
+    enforce(data.name).longerThan(2);
+  });
+  
+  test('price', 'This field is required.', () => {
+    enforce(data.price).isNotBlank();
+  });
+
+  test('price', 'Please provide valid price.', () => {
+    enforce(data.price).greaterThan(0);
+  });
+
+  test('description', 'This field is required.', () => {
+    enforce(data.description).isNotBlank();
+  });
+
+  test('description', 'Description is too long, Max 50 characters allowed.', () => {
+    enforce(data.description).shorterThan(51)
+  });
+
+  test('image', 'This field is required.', () => {
+    enforce(data.image).isNotBlank();
+  });
+
+});
+
+export default productSuite;
+
