30e08e7b0a5c87053649c40434fe58ca7bb9fe09
Jitendra Chaudhary <jitendrac.itpath@gmail.com>
2023-05-08 12:19:55 +0530

    # - 055 - change suite and remove enums.

--- src/validation/feedbackSuite.ts
+++ src/validation/feedbackSuite.ts
@@ -8,15 +8,23 @@ const feedbackSuite = create((data = {}, currentField) => {
     enforce(data.username).isNotBlank();
   });
 
-  test('username', 'Name must be at least 3 characters.', () => {
+  test('username', 'Please enter a valid name.', () => {
     enforce(data.username).longerThan(2);
+  });
+
+  test('username', 'Please enter a valid name.', () => {
+    enforce(data.username).matches(/^[^0-9]*$/);
+  });
+
+  test('username', 'Please enter a valid name.', () => {
+    enforce(data.username.trim()).equals(data.username);
   });
 
   test('email', 'This field is required.', () => {
     enforce(data.email).isNotBlank();
   });
 
-  test('email', ' Please enter a valid email address.', () => {
+  test('email', 'Please enter a valid email address.', () => {
     enforce(data.email).matches(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address.');
   });
   
@@ -28,6 +36,10 @@ const feedbackSuite = create((data = {}, currentField) => {
     enforce(data.message).shorterThan(51);
   });
   
+  test('message', 'Please enter a valid message', () => {
+    enforce(data.message.trim()).equals(data.message);
+  });
+
 });
 
 export default feedbackSuite;
